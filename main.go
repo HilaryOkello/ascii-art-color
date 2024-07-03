da717fec874876688a019c602b737219e4be1c02
hilaokello <hilaryokello091@gmail.com>
2024-07-03 11:00:54 +0300

    refactor: organising code

--- main.go
+++ main.go
@@ -14,29 +14,19 @@ var usage = fmt.Errorf(`Usage: go run . [OPTION] [STRING]
 
 EX: go run . --color=<color> "something"`)
 
-func main() {
-	var str string
-	var substr string
-	fileName := "standard.txt"
-
-	c := flag.String("color", "", "--color=<your color>")
-	if err := ascii.ValidateFlag(); err != nil {
-		fmt.Println(usage)
-		return
-	}
-	flag.Parse()
+func processArgs() (string, string, string, error) {
+	var str, substr string
+	fileName := "standard.txt" // default fileName
 
 	if flag.NFlag() == 0 {
 		switch flag.NArg() {
-		case 1:
+		case 1: // just single string
 			str = flag.Arg(0)
-		case 2:
+		case 2: // string + filename
 			str = flag.Arg(0)
 			fileName = flag.Arg(1) + ".txt"
 		default:
-			fmt.Println(usage)
-			return
-
+			return "", "", "", fmt.Errorf("%s", usage)
 		}
 	} else if flag.NFlag() == 1 {
 		switch flag.NArg() {
@@ -55,36 +45,32 @@ func main() {
 			str = flag.Arg(1)
 			fileName = flag.Arg(2) + ".txt"
 		default:
-			fmt.Println(usage)
-			return
+			return "", "", "", fmt.Errorf("%s", usage)
 
 		}
 	} else {
-		fmt.Print(usage)
+		return "", "", "", fmt.Errorf("%s", usage)
 	}
 	str = strings.ReplaceAll(str, "\\t", "    ")
 	str = strings.ReplaceAll(str, "\n", "\\n")
-	err := ascii.IsPrintableAscii(str)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	str = strings.ReplaceAll(substr, "\\t", "    ")
+	str = strings.ReplaceAll(substr, "\n", "\\n")
+	return str, substr, fileName, nil
+}
+
+func ReadBannerFile(fileName string) ([]string, error) {
 	// Set the second argument for the banner file name. the default has been set to standard.txt
 	filePath := os.DirFS("./banner")
 	contentByte, err := fs.ReadFile(filePath, fileName)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	if len(contentByte) == 0 {
-		fmt.Println("Banner file is empty")
-		return
+		return nil, fmt.Errorf("Banner file is empty")
 	}
 	// check if the banner file has been tampered with
-	er := ascii.CheckFileTamper(fileName, contentByte)
-	if er != nil {
-		fmt.Println(er)
-		return
+	if err = ascii.CheckFileTamper(fileName, contentByte); err != nil {
+		return nil, err
 	}
 	contentString := string(contentByte[1:])
 	if fileName == "thinkertoy.txt" {
@@ -92,7 +78,37 @@ func main() {
 		contentString = strings.ReplaceAll(string(contentByte[2:]), "\r\n", "\n")
 	}
 	contentSlice := strings.Split(contentString, "\n\n")
-	// Split the input string by "\\n" to get individual words
+	return contentSlice, nil
+}
+
+func main() {
+	// define a color flag
+	c := flag.String("color", "", "--color=<your color>")
+	if err := ascii.ValidateFlag(); err != nil {
+		fmt.Println(usage)
+		return
+	}
+	flag.Parse()
+
+	// process args passed
+	str, substr, fileName, err := processArgs()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// check if characters are printable
+	if err := ascii.IsPrintableAscii(str); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// get characters from the banner file
+	contentSlice, err := ReadBannerFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Split the str & substr by "\\n" to get the string section in each line
 	words := strings.Split(str, "\\n")
 	substrs := strings.Split(substr, "\\n")
 	count := 0
