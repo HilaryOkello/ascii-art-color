bf3d740d22043ebb800df53a620459faa5a5116f
hilaokello <hilaryokello091@gmail.com>
2024-07-08 16:42:10 +0300

    test: added test for shouldPrintWithColor func

--- ascii/printascii_test.go
+++ ascii/printascii_test.go
@@ -1,6 +1,8 @@
 package ascii
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetIndices(t *testing.T) {
 	testCases := []struct {
@@ -70,4 +72,46 @@ func equalSlices(a, b []int) bool {
 		}
 	}
 	return true
+}
+
+func Test_shouldPrintWithColor(t *testing.T) {
+	type args struct {
+		args    *PrintArgs
+		indices []int
+		track   int
+		i       int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Color but No Substring",
+			args: args{args: &PrintArgs{Color: "31", Str: "Hello World", Substr: ""}, indices: []int{}, track: 0, i: 0},
+			want: true,
+		},
+		{
+			name: "Substring Match 01",
+			args: args{args: &PrintArgs{Color: "31", Str: "hello world", Substr: "hello"}, indices: []int{0}, track: 0, i: 3},
+			want: true,
+		},
+		{
+			name: "Substring Match But Index Not Colored",
+			args: args{args: &PrintArgs{Color: "31", Str: "hello world", Substr: "world"}, indices: []int{6}, track: 0, i: 3},
+			want: false,
+		},
+		{
+			name: "Multiple Indices",
+			args: args{args: &PrintArgs{Color: "31", Str: "hello world hello", Substr: "hello"}, indices: []int{0, 12}, track: 1, i: 13},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shouldPrintWithColor(tt.args.args, tt.args.indices, tt.args.track, tt.args.i); got != tt.want {
+				t.Errorf("shouldPrintWithColor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
